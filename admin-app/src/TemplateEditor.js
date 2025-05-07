3d8e04f6bb5eddbb3ae5ed9b5e26d8cc1a4a0a22
Martin Zwernemann <52236002+Zwernemann@users.noreply.github.com>
2025-05-07 17:22:22 +0200

    Update TemplateEditor.js

--- admin-app/src/TemplateEditor.js
+++ admin-app/src/TemplateEditor.js
@@ -1,35 +1,33 @@
+import React, { useEffect } from 'react';
 import grapesjs from 'grapesjs';
 import 'grapesjs/dist/css/grapes.min.css';
 
 import presetWebpage from 'grapesjs-preset-webpage';
 import pluginForms from 'grapesjs-plugin-forms';
-
 import parserPostCSS from 'grapesjs-parser-postcss';
 
-useEffect(() => {
-  grapesjs.init({
-    container: '#gjs',
-    height: '100vh',
-    width: 'auto',
-    fromElement: false,
-    components: '',
-    style: '',
-    storageManager: false,
-    plugins: [presetWebpage, pluginForms, parserPostCSS],
-    pluginsOpts: {
-      [presetWebpage]: {},
-      [pluginForms]: {},
-      [parserPostCSS]: {}
-    }
-  });
-}, []);
+export default function TemplateEditor() {
+  useEffect(() => {
+    grapesjs.init({
+      container: '#gjs',
+      height: '100vh',
+      width: 'auto',
+      fromElement: false,
+      components: '',
+      style: '',
+      storageManager: false,
+      plugins: [presetWebpage, pluginForms, parserPostCSS],
+      pluginsOpts: {
+        [presetWebpage]: {},
+        [pluginForms]: {},
+        [parserPostCSS]: {}
+      }
+    });
+  }, []);
 
   return (
     <div className="bg-white">
-      <div id="gjs" className="bg-white border h-full">
-        <h1>Start your template here</h1>
-        <p>Drag and drop components from the sidebar</p>
-      </div>
+      <div id="gjs" className="h-screen" />
     </div>
   );
 }
